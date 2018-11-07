415baf3ee61c9b3844a210b531825ee9a88251d6
jitheima <2998429686@qq.com>
2018-11-07 19:48:03 +0800

    init ...

--- vue/src/main/java/com/itheima/Test.java
+++ vue/src/main/java/com/itheima/Test.java
@@ -8,5 +8,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello");
+
+        System.out.println("master....");
+        System.out.println("master....");
+        System.out.println("master....");
     }
 }
