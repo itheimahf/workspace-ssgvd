e27d8b846293b9f402d821dcfc4059887315861f
jitheima <2998429686@qq.com>
2018-11-07 19:43:56 +0800

    init ...

---
+++ vue/src/main/java/cn/itcast/demo/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast.demo;
+
+/**
+ * @author NQ
+ * @create 2018-11-07 19:43
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world....");
+    }
+}
